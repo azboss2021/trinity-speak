eb0b94f1f718add600d5eb5937cc4df14f10586e
azboss2021 <alexanderzyn1@gmail.com>
2024-04-10 12:31:50 -0700

    Add sticky navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <nav className="sticky top-0 flex justify-between bg-base-100 py-4">
+      <Link href="/">
+        <h1 className="text-xl">✝️ TrinitySpeak</h1>
+      </Link>
+    </nav>
+  );
+};
+export default Navbar;
